30c31c261df0f5816211b49966316f08dd80baec
Alexandra Boedtker <alexboedtker@gmail.com>
2021-04-14 14:31:13 +0200

    added model, controller and first view of index with some stlying

---
+++ app/controllers/flats_controller.rb
@@ -0,0 +1,14 @@
+class FlatsController < ApplicationController
+  def index
+    @flats = Flat.all
+  end
+
+  def edit
+  end
+
+  def new
+  end
+
+  def show
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database..."
+Flat.destroy_all
+
+puts "Creating flats..."
+
+4.times do
+  flat = Flat.create(
+    name: Faker::Music::Hiphop.artist,
+    address: Faker::Address.street_name,
+    description: Faker::Marketing.buzzwords,
+    price_per_night: [75, 100, 125, 150, 200].sample,
+    number_of_guests: [2, 3, 4, 5].sample
+    )
+  puts "flat #{flat.id} is created"
+end
+
+puts "Finished!"
