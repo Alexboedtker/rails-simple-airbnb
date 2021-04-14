3c705f0015b80dbdea7c5ab47025cd6eed8e432d
Alexandra Boedtker <alexboedtker@gmail.com>
2021-04-14 15:36:24 +0200

    created new form plus styling

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -7,8 +7,20 @@ class FlatsController < ApplicationController
   end
 
   def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.create(flat_params)
+    redirect_to flat_path(@flat)
   end
 
   def show
+  end
+
+  private
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :number_of_guests, :price_per_night)
   end
 end
